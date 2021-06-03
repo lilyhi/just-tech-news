009156646da3f2ec2da0ec8d43f4e8729f8e8e21
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-06-03 15:53:06 -0700

    added post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,17 @@
+// import all models
 const User = require('./User');
 const Post = require('./Post');
+
+// create assocations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+// reverse assocation
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+
 
 module.exports = { User, Post };
